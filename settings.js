25065089fd37833c69b09b0ecc477edfb3ca9f6d
wira09 <zmuhamad064@gmail.com>
2023-12-10 15:12:54 +0700

    first commit

---
+++ settings.js
@@ -0,0 +1,190 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.antispam = false//auto kick spammer (true to on, false to off)
+
+global.APIs = {
+
+    // API Prefix
+
+    // name: 'https://website'
+    xteam: 'https://api.xteam.xyz',
+    lol: 'https://api.lolhuman.xyz',
+    males: 'https://malesin.xyz',
+    zein: 'https://api.zahwazein.xyz',
+    rose: 'https://api.itsrose.life',
+    xzn: 'https://xzn.wtf',
+    saipul: 'https://saipulanuar.cf'
+}
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "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"
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['6283841407959'] //ur owner number
+global.ownernomer = "6283841407959" //ur owner number2
+global.ownername = "Wira" //ur owner name
+global.ytname = "YT: SanZxyz1" //ur yt chanel name
+global.socialm = "GitHub: AhmadDaniWn" //ur github or insta name
+global.location = "Bandung, Jabar, Indonesia" //ur location
+
+//Server crete panel egg biasa
+global.domain = 'https://satzhost.panellstore.my.id' // Isi Domain Lu
+global.apikey = 'ptla_9RxOT3in1YTcUXfdOrqguCf04L0wBPF3WWy10NMYSmT' // Isi Apikey Plta Lu
+global.capikey = 'ptlc_4XK7zPgWFiGuEw8uP0h0BNLDxAan0flOtfnaRgMyXlA' // Isi Apikey Pltc Lu
+//===========================//
+//Server create panel egg pm2
+global.apikey2 = '-' // Isi Apikey Plta Lu
+global.capikey2 = '-' // Isi Apikey Pltc Lu
+global.domain2 = '-' // Isi Domain Lu
+global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //jangan di ubah
+
+global.eggsnya2 = '15' // id eggs yang dipakai
+global.location2 = '1' // id location
+//===========================//
+global.domainotp = "https://claudeotp.com/api"
+global.apikeyotp = "a395f97fe99f4fad0e790d10af518b9a"
+global.eggsnya = '15' // id eggs yang dipakai
+global.location3 = '1' // id location
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+//===========================//
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '🪙',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+//new
+global.botname = "Sanzz Botz⚡"
+global.ownernumber = '6283893169108'
+global.ownername = 'SanZ Owner'
+global.ownerNumber = ["6283893169108@s.whatsapp.net"]
+global.ownerweb = ""
+global.websitex = ""
+global.wagc = "https://chat.whatsapp.com/JzXpGWhy6FT0B3evbjYiA9"
+global.themeemoji = '🪀'
+global.wm = "Sanz Bot Inc."
+global.botscript = 'Dah gede nyari sc 🗿🖕' //script link
+global.packname = "Sticker By"
+global.author = "| Sanz X Ciya Botz |\n\n"
+global.creator = "6283893169108@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Ini dia!!', 
+wait: "Wait Kak Lagi Proses",
+   success: "Sukses Kak`",
+   on: "Sudah Aktif", 
+   off: "Sudah Off",
+   query: {
+       text: "Teks Nya Mana Kak?",
+       link: "Link Nya Mana Kak?",
+   },
+   error: {
+       fitur: "Mohon Maaf Kak Fitur Eror Silahkan Chat Developer Bot Agar Bisa Segera Diperbaiki",
+   },
+   only: {
+       group: "Maaf Kak Fitur Ini Hanya Bisa Digunakan Di Dalam Group",
+       private: "Maaf Kak Fitur Ini Hanya Bisa Digunakan Di Dalam Private Chat",
+       owner: "Maaf Kak Fitur Ini Hanya Bisa Digunakan Sama Owner Bot",
+       admin: "Maaf Kak Fitur Ini Hanya Bisa Digunakan Sama Owner Bot",
+       badmin: "Maaf Kak Kaya Nya Kakak Tidak Bisa Menggunakan Fitur Ini Di Karenakan Bot Bukan Admin Group",
+       premium: "Maaf Kamu Belum Jadi User Premium Untuk Menjadi User Premium Silahkan Beli Ke Owner Dengan Cara Ketik .owner",
+   }
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
